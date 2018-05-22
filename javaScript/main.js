a45b87a20fdbe9f6330db5c3edb918a63d210f7f
blkty23 <gtjames1867@gmail.com>
2018-05-22 17:47:49 -0400

    Ltr_argument

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,6 +1,24 @@
-Let words={"Rose", "boss","poop","Registration","Drizzy",
-"HYFR","Humble","Love","DNA","papa","Brooklyn","blueprint","jigga","allure"};
+let words = ["Rose", "Boss","Poop","Registration","Drizzy","HYFR","Humble","Love","DNA","Papa","Brooklyn","Blueprint","Jigga","Allure"];
 
-const rand = math.floor(math.random()*words.length);
-const current word = words[rand];
-console.log(currentword.spaces);
+const rand = Math.floor(Math.random()*words.length);
+const currentWord = words[rand];
+console.log(currentWord);
+
+
+
+function button(x){
+    console.log(button)
+    var n = currentWord.includes(x)
+
+        if(currentWord.includes==value[x]){
+
+       
+            console.log('button clicked')
+            document.getElementById('pic2c').src = currentWord
+
+        }
+        else if (currentWord !== value[x]){
+
+       
+        }
+}
