c38d5bdbb7d6b8df0ba058c064114709e3adb079
blkty23 <gtjames1867@gmail.com>
2018-05-22 15:11:43 -0400

    word select

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,1 +1,6 @@
-function words={"Rose", "boss","poop","Registration","Drizzy","HYFR","Humble","Love","DNA","papa","Brooklyn"}
+Let words={"Rose", "boss","poop","Registration","Drizzy",
+"HYFR","Humble","Love","DNA","papa","Brooklyn","blueprint","jigga","allure"};
+
+const rand = math.floor(math.random()*words.length);
+const current word = words[rand];
+console.log(currentword.spaces);
