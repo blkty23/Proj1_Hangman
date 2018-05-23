53453afdb91a85075bdbb77d210a02bb8b755cc6
blkty23 <gtjames1867@gmail.com>
2018-05-23 11:32:08 -0400

    stringdsply

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,24 +1,29 @@
 let words = [{
 wrdCh: "Rose",
 wrdSpcs: 4,
-wrdltr1: R,
-Wrdltr2: o,
-Wrdltr3: s,
-wrdltr4: e
+wrdltr1: "R",
+Wrdltr2: "o",
+Wrdltr3: "s",
+wrdltr4: "e",
 wrddsply: function() {
-    console.log("_ _ _ _");
+    let strgdsply = "_ _ _ _"
+    return(strgdsply);
   }
-},
+},];
 
-, "Boss","Poop","Registration","Drizzy","HYFR","Humble","Love","DNA","Papa","Brooklyn","Blueprint","Jigga","Allure"];
+// , "Boss","Poop","Registration","Drizzy","HYFR","Humble","Love","DNA","Papa","Brooklyn","Blueprint","Jigga","Allure"];
 
 
 // let words = ["Rose","Boss","Poop","Registration","Drizzy","HYFR","Humble","Love","DNA","Papa","Brooklyn","Blueprint","Jigga","Allure"];
-const rand = Math.floor(Math.random()*words.length);
-const currentWord = words[rand];
-
-console.log(currentWord);
-
+//const rand = Math.floor(Math.random()*words.length); //Selects a random word from the word list
+// const currentWord = words[rand];
+const currentWord = words[0];
+console.log(currentWord.wrdCh);
+const wrdshw = document.createElement("P");                       // Create a <p> element
+const t = document.createTextNode(words[0].wrddsply());       // Create a text node
+wrdshw.appendChild(t);                                          // Append the text to <p>
+//document.div.pic2c.appendChild(wrdshw);                              // Append <p> to <body>
+document.getElementById("spot4a").appendChild(wrdshw);
 
 
 function button(x){
@@ -40,6 +45,6 @@ function button(x){
         }
 }
 
-    for(var i=0, i<=currentWord.length, i++){
-        if(i==)
-}
+//     for(var i=0, i<=currentWord.length, i++){
+//         if(i==)
+// }
