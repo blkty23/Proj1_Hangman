4e8582470b8a47490a298bef58b864746802d28e
blkty23 <gtjames1867@gmail.com>
2018-05-23 15:22:09 -0400

    functions

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,3 +1,39 @@
+let attmpt= 6;
+document.getElementById("spotAtmpt").appendChild(attmpt)
+function attmptcalc(){
+    //loop through word
+    //if letter matches word[i]
+    //reveal letter in strgdsply
+    //if letter is not in word
+    //attmpt--
+    //disable letter used: $(event.target).off() 
+    for (var i = 0; i < currentWord.length; i++)
+    {   
+        //
+        var c = currentWord.charAt(i);
+        console.log(c);
+        wrdchk();
+    }
+}
+
+let attmpt = 6
+let words = "cool"
+function wrdchk(x) {
+        var str = x;
+        var n = str.indexOf("e");
+        //document.getElementById("spotAtmpt").innerHTML = n;
+
+        if(n === -1){
+          attmpt = attmpt - 1
+        }
+        console.log(attmpt);
+        console.log(n);
+}
+wrdchk(words);
+
+
+
+
 let words = [{
 wrdCh: "Rose",
 wrdSpcs: 4,
@@ -12,11 +48,11 @@ wrddsply: function() {
 },];
 
 // , "Boss","Poop","Registration","Drizzy","HYFR","Humble","Love","DNA","Papa","Brooklyn","Blueprint","Jigga","Allure"];
-
-
 // let words = ["Rose","Boss","Poop","Registration","Drizzy","HYFR","Humble","Love","DNA","Papa","Brooklyn","Blueprint","Jigga","Allure"];
 //const rand = Math.floor(Math.random()*words.length); //Selects a random word from the word list
 // const currentWord = words[rand];
+
+
 const currentWord = words[0];
 console.log(currentWord.wrdCh);
 const wrdshw = document.createElement("P");                       // Create a <p> element
